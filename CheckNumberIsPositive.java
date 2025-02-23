c6e3de81b1a41d194c2947679256c75e2d121f21
vhema06 <144662955+vhema06@users.noreply.github.com>
2025-02-23 12:23:39 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,20 @@
+package WeekOneDayThree;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+
+		int a = 15;
+		if (a > 0) {
+			System.out.println("Postive number");
+
+		} else if (a < 0) {
+			System.out.println("negative number");
+
+		} else {
+			System.out.println("valueis zero");
+		}
+
+	}
+
+}
