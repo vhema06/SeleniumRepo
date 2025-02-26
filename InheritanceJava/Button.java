bd77eaf18c711fa6a913bf7145fc5b09f0a34694
vhema06 <144662955+vhema06@users.noreply.github.com>
2025-02-26 17:20:19 +0530

    Add files via upload

---
+++ InheritanceJava/Button.java
@@ -0,0 +1,15 @@
+package InheritanceJava;
+
+public class Button extends WebElement {
+	
+	public void submit() {
+	}
+	public static void main(String[] args) {
+		
+		Button but = new Button();
+		but.submit();
+		but.click();
+		but.setText();	
+	}
+
+}
---
+++ InheritanceJava/RadioButton.java
@@ -0,0 +1,17 @@
+package InheritanceJava;
+
+public class RadioButton extends Button {
+
+	public void SelectRadioButton() {
+
+	}
+
+	public static void main(String[] args) {
+
+		RadioButton Radio = new RadioButton();
+		Radio.SelectRadioButton();
+		Radio.submit();
+
+	}
+
+}
